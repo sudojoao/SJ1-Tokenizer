22182f50e68e751eb4a81a72a1202e1f6b1444c6
João Vieira <162704766+sudojoao@users.noreply.github.com>
2025-01-30 17:29:11 +0000

    post-processing

--- tokenizer.py
+++ tokenizer.py
@@ -2,6 +2,7 @@ from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.processors import TemplateProcessing
 
 
 tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
@@ -17,3 +18,15 @@ tokenizer.train(files, trainer)
 tokenizer.save("data/tokenizer-lib.json")
 
 tokenizer = Tokenizer.from_file("data/tokenizer-lib.json")
+
+tokenizer.post_processor = TemplateProcessing(
+    single="[CLS] $A [SEP]",
+    pair="[CLS] $A [SEP] $B:1 [SEP]:1",
+    special_tokens=[
+        ("[CLS]", tokenizer.token_to_id("[CLS]")),
+        ("[SEP]", tokenizer.token_to_id("[SEP]")),
+    ],
+)
+
+output = tokenizer.encode("Hello, y'all! How are you 😁 ?")
+print(output.tokens)
